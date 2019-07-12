a8196b1bcadca5f1cd1aac03c724908c82646246
admin <admin@admins-MacBook-Pro.local>
2019-07-12 14:30:30 -0500

    fixed name

--- src/test/java/com/quickscrim/quickscrim/QuickscrimApplicationTests.java
+++ src/test/java/com/quickscrim/quickscrim/QuickscrimApplicationTests.java
@@ -1,4 +1,4 @@
-package com.quickscrim.quicksrim;
+package com.quickscrim.quickscrim;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class QuicksrimApplicationTests {
+public class QuickscrimApplicationTests {
 
     @Test
     public void contextLoads() {
