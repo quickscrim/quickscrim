ca36687ae89fc49199beb600e40738cb7304f943
Harley Garcia <orion@Harleys-MacBook-Air.local>
2019-07-12 11:16:32 -0500

    first commit

---
+++ src/main/java/com/quickscrim/quicksrim/QuicksrimApplication.java
@@ -0,0 +1,13 @@
+package com.quickscrim.quicksrim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicksrimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuicksrimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quickscrim/quicksrim/QuicksrimApplicationTests.java
@@ -0,0 +1,16 @@
+package com.quickscrim.quicksrim;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuicksrimApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
