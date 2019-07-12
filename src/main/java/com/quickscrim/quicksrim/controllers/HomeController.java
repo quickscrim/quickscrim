5a06477fa8d4d9ff6a8858fff9b6040963ef2ca0
admin <admin@admins-MacBook-Pro.local>
2019-07-12 12:05:59 -0500

    working landing page

--- src/main/java/com/quickscrim/quicksrim/controllers/HomeController.java
+++ src/main/java/com/quickscrim/quicksrim/controllers/HomeController.java
@@ -2,12 +2,14 @@ package com.quickscrim.quicksrim.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
+    @ResponseBody
     public String index() {
         return "landing page";
     }
