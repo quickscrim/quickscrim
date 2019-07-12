8d3b9a4f2d06988d5b046ea1d8979b12e804e5f2
Harley Garcia <orion@Harleys-MacBook-Air.local>
2019-07-12 13:10:08 -0500

    user model

--- src/main/java/com/quickscrim/quickscrim/models/User.java
+++ src/main/java/com/quickscrim/quickscrim/models/User.java
@@ -1,45 +1,144 @@
 package com.quickscrim.quickscrim.models;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table
 public class User {
 
     @Id
     @GeneratedValue
     private long id;
 
-    @Column(nullable = false, length = )
+    @Column(nullable = false, length = 24, unique = true)
     private String username;
 
-
+    @Column(nullable = false, unique = true)
     private String email;
 
-
+    @Column(nullable = false)
     private String password;
 
-
+    @Column(nullable = false)
     private Boolean isAdmin;
 
-
+    @Column(nullable = false)
     private String firstname;
 
-
+    @Column(nullable = false)
     private String lastname;
 
-
+    @Column(nullable = false)
     private String city;
 
-
+    @Column(nullable = false)
     private String State;
 
-
+    @Column(nullable = false)
     private String image;
 
-
+    @Column(nullable = false, length = 1000)
     private String bio;
+
+    public User(String username, String email, String password, Boolean isAdmin, String firstname, String lastname, String city, String state, String image, String bio) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.isAdmin = isAdmin;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.city = city;
+        this.State = state;
+        this.image = image;
+        this.bio = bio;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String state) {
+        State = state;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
 }
