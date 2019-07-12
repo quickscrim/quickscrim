8a3111a3bcf56e9a11fbbdb3c2e962d1706faea4
Harley Garcia <orion@Harleys-MacBook-Air.local>
2019-07-12 11:42:04 -0500

    db

---
+++ src/main/java/com/quickscrim/quicksrim/models/User.java
@@ -0,0 +1,45 @@
+package com.quickscrim.quicksrim.models;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+
+    private String username;
+
+
+    private String email;
+
+
+    private String password;
+
+
+    private Boolean isAdmin;
+
+
+    private String firstname;
+
+
+    private String lastname;
+
+
+    private String city;
+
+
+    private String State;
+
+
+    private String image;
+
+
+    private String bio;
+}
