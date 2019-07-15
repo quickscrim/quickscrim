8845b1f463d93b23960aa467a2a0a5784dae79e0
Edwin Buendia <edwinbuendia@Edwins-MacBook-Pro.local>
2019-07-15 12:35:00 -0500

    register page branch and view

---
+++ src/main/java/com/quickscrim/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.quickscrim.controllers;
+
+import com.quickscrim.models.User;
+import com.quickscrim.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+    private UserRepository userDao;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
+        this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("user/register")
+    public String register(Model model) {
+        model.addAttribute("user", new User());
+        return "user/register";
+    }
+
+    @PostMapping("/register")
+    public String saveUser(@ModelAttribute User user) {
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/login";
+    }
+
+}
