3dcf04120422f7071bd558260f190f9aba01b56c
Harley Garcia <orion@Harleys-MacBook-Air.local>
2019-07-15 16:53:24 -0500

    post stuff

--- src/main/java/com/quickscrim/controllers/ForumController.java
+++ src/main/java/com/quickscrim/controllers/ForumController.java
@@ -1,7 +1,9 @@
 package com.quickscrim.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ForumController {
@@ -11,9 +13,16 @@ public class ForumController {
         return "forum/category";
     }
 
-    @GetMapping("forum/posts{category}")
-        public String Posts() {
-        return "forum/posts";
+    @GetMapping("forum/posts/{category}")
+        public String Posts(@PathVariable String category, Model model) {
+        model.addAttribute("category", category);
+        return "showposts";
     }
+
+    @GetMapping("forum/post/create")
+    public String creatPost() {
+        return "createpost";
+    }
+
 
 }
