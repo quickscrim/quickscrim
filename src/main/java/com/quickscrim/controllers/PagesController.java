38554825fa395cf2c2c20514b4853d28b68ee32e
Nathaniel Dowell <dowellnathaniel@yahoo.com>
2019-07-18 13:54:12 -0500

    pages controller and basic pages for terms, privacy, and about us

--- src/main/java/com/quickscrim/controllers/PagesController.java
+++ src/main/java/com/quickscrim/controllers/PagesController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class StaticController {
+public class PagesController {
     @GetMapping("pages/about")
     public String about() {
         return "pages/about";
