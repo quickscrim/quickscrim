69d9ef762fc0b913905dc08f2d2b86cbb5d30966
Edwin Buendia <buendiaedwin2@gmail.com>
2019-07-22 14:58:50 -0500

    events for user only

--- src/main/java/com/quickscrim/repositories/EventRepository.java
+++ src/main/java/com/quickscrim/repositories/EventRepository.java
@@ -6,11 +6,11 @@ import com.quickscrim.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.lang.model.element.ElementVisitor;
 import java.util.List;
 
 
 @Repository
 public interface EventRepository extends CrudRepository<Event, Long> {
-    List<Event> findAllByEventSport(List<Category> categories);
-    List<Event> findAllByEventCreator();
+    List<Event> findAllByEventCreator(User user);
 }
