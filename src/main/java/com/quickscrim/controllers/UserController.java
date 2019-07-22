9fcf03ad10c486d518434ce79a297ea19827964f
Harley Garcia <Harley.Garcia36@gmail.com>
2019-07-22 14:53:44 -0500

    profile edit

--- src/main/java/com/quickscrim/controllers/UserController.java
+++ src/main/java/com/quickscrim/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.quickscrim.controllers;
 
 import com.quickscrim.models.User;
 import com.quickscrim.repositories.UserRepository;
+import com.quickscrim.services.UserDetailsLoader;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,10 +23,12 @@ import javax.validation.Valid;
 public class UserController {
     private UserRepository users;
     private PasswordEncoder passwordEncoder;
+    private UserDetailsLoader userService;
 
-    public UserController(UserRepository users, PasswordEncoder passwordEncoder) {
+    public UserController(UserRepository users, PasswordEncoder passwordEncoder, UserDetailsLoader userService) {
         this.users = users;
         this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
     }
 
     @GetMapping("/register")
@@ -44,12 +48,5 @@ public class UserController {
         users.save(user);
         return "redirect:/login";
     }
-
-    @GetMapping("user/profile")
-    public String displayUserProfile () {
-
-            return "user/profile";
-    }
-
 
 }
--- src/main/java/com/quickscrim/repositories/PostRepository.java
+++ src/main/java/com/quickscrim/repositories/PostRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
 
+//    List<Post>findByPostCategory();
 
 
 }
