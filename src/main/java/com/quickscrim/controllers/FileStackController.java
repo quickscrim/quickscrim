2540773bfac9d2512d8ee55d79405c98ff59e67f
Nathaniel Dowell <dowellnathaniel@yahoo.com>
2019-07-22 15:05:05 -0500

    commit before pull

--- src/main/java/com/quickscrim/controllers/FileStackController.java
+++ src/main/java/com/quickscrim/controllers/FileStackController.java
@@ -2,6 +2,8 @@ package com.quickscrim.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FileStackController {
@@ -9,5 +11,12 @@ public class FileStackController {
     @GetMapping ("/upload/pic")
     public String displayPickerPage () {
         return "filestacktest";
+    }
+
+    @PostMapping ("/upload/pic")
+    public String postProfilePic(@RequestParam("profilePicUrl") String profilePicUrl) {
+//        find user, set user pic attribute to profilePicUrl
+
+        return "redirect:user/profile";
     }
 }
