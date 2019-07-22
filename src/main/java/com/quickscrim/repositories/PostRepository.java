c6bb97d1d3f7a22b634c228c8ecdd524d3abb81b
admin <mattbutler2536@live.com>
2019-07-22 09:26:32 -0500

    fixed auto complete not working 100 percent of the time

--- src/main/java/com/quickscrim/repositories/PostRepository.java
+++ src/main/java/com/quickscrim/repositories/PostRepository.java
@@ -3,10 +3,12 @@ package com.quickscrim.repositories;
 import com.quickscrim.models.Post;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 
+@Repository
 public interface PostRepository extends CrudRepository<Post, Long> {
 
 
